1a70bacaa16562355b9a1d39544066a424b96890
Lea732 <leapoulin07@gmail.com>
2024-04-02 10:44:30 +0200

    Added new const in title service

--- frontend/src/services/titleService.js
+++ frontend/src/services/titleService.js
@@ -14,9 +14,36 @@ export const addTitle = (checklistTitle) => {
     .catch((error) => console.error(error));
 };
 
+export const readTitle = () => {
+  return client
+    .get("/title")
+    .then((response) => response.data)
+    .catch((error) => console.error(error));
+};
+
+export const readTitleById = (id) => {
+  return client
+    .get(`/title/${id}`)
+    .then((response) => response.data)
+    .catch((error) => console.error(error));
+};
+
+export const updateTitle = (id, title) => {
+  return client
+    .put(`/title/${id}`, title)
+    .then((response) => console.info(response))
+    .catch((error) => console.error(error));
+};
+
 export const deleteTitle = (id) => {
   return client
     .delete(`/title/${id}`)
-    .then((response) => console.info(response))
-    .catch((error) => console.error(error));
+    .then((response) => {
+      console.info("Title deleted successfully:", response.data);
+      return response.data;
+    })
+    .catch((error) => {
+      console.error("Error deleting title:", error);
+      throw error;
+    });
 };
